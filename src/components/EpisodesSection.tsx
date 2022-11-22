4d4264ae1276104e1e83fdb8aecc004fb33987c9
Eric Lich <eric.lichtensztein@aleia.io>
2022-11-22 10:00:14 -0300

    add lazy and suspense for episodes section

--- src/components/EpisodesSection.tsx
+++ src/components/EpisodesSection.tsx
@@ -1,8 +1,11 @@
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, lazy, Suspense } from "react";
 
 import { CharacterContext } from "../context/characterContext";
-import EpisodeIndividual from "./EpisodeIndividual";
-import SharedEpisodes from "./SharedEpisodes";
+/* import EpisodeIndividual from "./EpisodeIndividual";
+import SharedEpisodes from "./SharedEpisodes"; */
+
+const EpisodeIndividual = lazy(() => import("./EpisodeIndividual"));
+const SharedEpisodes = lazy(() => import("./SharedEpisodes"));
 
 const EpisodesSection = () => {
   const { character1, character2 } = useContext(CharacterContext);
@@ -10,11 +13,15 @@ const EpisodesSection = () => {
   const renderEpisodesSection = useMemo(() => {
     if (character1 && character2) {
       return (
-        <>
+        <Suspense
+          fallback={
+            <h2 className="m-auto text-main font-sono drop-shadow-fontShadow">Loading...</h2>
+          }
+        >
           <EpisodeIndividual selectedCharacter={character1} otherCharacterId={character2.id} />
           <SharedEpisodes />
           <EpisodeIndividual selectedCharacter={character2} otherCharacterId={character1.id} />
-        </>
+        </Suspense>
       );
     }
   }, [character1, character2]);
